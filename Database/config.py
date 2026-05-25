63b9ff7e8fdb54a3c97fa642b4831065c8645076
shandiaye <sokhnaaichaa02@gmail.com>
2026-05-25 22:03:27 +0000

    Ajout de config.py et connexion.py (pattern Singleton)

---
+++ Database/config.py
@@ -0,0 +1,9 @@
+#ici on va gerer la configuration de la connextion a la base de donnees
+
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'user': 'postgres',
+    'password': 'aicha',
+    'database': 'gestion_incident'
+}
